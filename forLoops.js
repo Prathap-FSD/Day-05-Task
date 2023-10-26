a3a8fcc0440ec712f534e7add1662aae281d2c73
Prathap <143264769+Prathap-FSD@users.noreply.github.com>
2023-10-26 07:35:16 +0530

    Update forLoops.js

--- forLoops.js
+++ forLoops.js
@@ -4,24 +4,24 @@ let jsonObject = JSON.parse(jsonString)
 
 //!  For loop ---------------!//
 
-// for(let i = 0; i<(Object.keys(jsonObject)).length; i++){
-//     console.log(Object.keys(jsonObject)[i], ':' , jsonObject[Object.keys(jsonObject)[i]] );
-// }
+     for(let i = 0; i<(Object.keys(jsonObject)).length; i++){
+     console.log(Object.keys(jsonObject)[i], ':' , jsonObject[Object.keys(jsonObject)[i]] );
+ }
 
 //!  For in ---------------!//
 
-// for(let i in Object.keys(jsonObject)){
-//     console.log(Object.keys(jsonObject)[i], ':', jsonObject[Object.keys(jsonObject)[i]]);
-// }
+     for(let i in Object.keys(jsonObject)){
+     console.log(Object.keys(jsonObject)[i], ':', jsonObject[Object.keys(jsonObject)[i]]);
+ }
 
 //!  For of ---------------!//
 
-// for(let pair of Object.entries(jsonObject)){
-//     console.log(pair[0],':',pair[1]);
-// }
+     for(let pair of Object.entries(jsonObject)){
+     console.log(pair[0],':',pair[1]);
+ }
 
 //!  For Each ---------------!//
 
-// Object.entries(jsonObject).forEach((entry)=>{
-//     console.log(entry[0],':', entry[1]);
-// })
+     Object.entries(jsonObject).forEach((entry)=>{
+     console.log(entry[0],':', entry[1]);
+ })
